e43fe137a20761193bb6e8d3731e8f57500162f9
nirsis <jeroferraro@gmail.com>
2021-03-02 11:28:41 +0000

    asd

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const http = require('http');
 const axios = require('axios');
+const logger = require('morgan');
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parse');
 
-http.createServer((req, res)=>{
-  res.write("Hello BScBest! \n"); // write a response
-  res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+var app = express();
+var port = 8000;
 
+//http.createServer((req, res) => {
+//    res.write(users.join("\n")); // write a response
+  //  res.write(emails.join("\n"));
+    // res.end(); //end the response
+// }).listen(8000); // listen for requests on port 8000
+
+app.get(' /hello', (req, res) => {
+    res.write("Hello BSCBest!");
+    res.write(users.join('\n'));
+}
 let users = []; // names of users will be stored here
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(({ data }) => {
-      users = data.map(user => user.name); // get only the names of the users and store in an array
-    })
-    .catch(error=>{
-      console.log(error);
-    });
+
+(async function getNames() {
+    try {
+        const { data } = await axios.get("https://swapi.dev/api/people/");
+        users = data.results.map(user => user.name);
+       
+
+        console.log(users)
+        
+    } catch (error) {
+        console.log(error)
+    }
+})()
+
